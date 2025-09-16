c017511c0b79ba7fadd9ed31c5b153c824bc06e7
HRITHIK227 <121802377+HRITHIK227@users.noreply.github.com>
2025-09-16 20:08:55 +0530

    Add files via upload

---
+++ kashmir.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+<style>
+.img-fullwidth {
+  width: 100%;
+  height: 250px;      
+  object-fit: cover;  
+  border-radius: 6px; }
+</style>
+
+
+<!-- Meta Tags -->
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<meta name="description" content="Learnpro - Education University School Kindergarten Learning HTML Template" />
+<meta name="keywords" content="education,school,university,educational,learn,learning,teaching,workshop" />
+<meta name="author" content="ThemeMascot" />
+
+<!-- Page Title -->
+<title>Kashmir</title>
+
+<!-- Favicon and Touch Icons -->
+<link href="images/jk.png" rel="shortcut icon" type="image/png">
+<link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+<link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+<link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+<!-- Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/css-plugin-collections.css" rel="stylesheet"/>
+<!-- CSS | menuzord megamenu skins -->
+<link href="css/menuzord-megamenu.css" rel="stylesheet"/>
+<link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-boxed.css" rel="stylesheet"/>
+<!-- CSS | Main style file -->
+<link href="css/style-main.css" rel="stylesheet" type="text/css">
+<!-- CSS | Preloader Styles -->
+<link href="css/preloader.css" rel="stylesheet" type="text/css">
+<!-- CSS | Custom Margin Padding Collection -->
+<link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+<!-- CSS | Responsive media queries -->
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+<!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+<!-- CSS | Theme Color -->
+<link href="css/colors/theme-skin-color-set1.css" rel="stylesheet" type="text/css">
+
+<!-- external javascripts -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- JS | jquery plugin collection for this theme -->
+<script src="js/jquery-plugin-collection.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body class="">
+<div id="wrapper" class="clearfix">
+  <!-- preloader -->
+
+  
+  <!-- Header -->
+  <header id="header" class="header">
+    <div class="header-top sm-text-center" style="background: linear-gradient(to right, #43cea2, #185a9d);">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="widget text-white">
+              <ul class="list-inline xs-text-center text-white">
+                <li class="m-0 pl-10 pr-10"> <a href="#" class="text-white"><i class="fa fa-phone text-white"></i> 8492840864</a> </li>
+                <li class="m-0 pl-10 pr-10"> 
+                  <a href="#" class="text-white"><i class="fa fa-envelope-o text-white mr-5"></i> <span class="__cf_email__" data-cfemail="">hrithiks227@gmail.com</span></a> 
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4 pr-0">
+            <div class="widget">
+               <ul class="styled-icons icon-sm pull-right flip sm-pull-none sm-text-center mt-5">
+                <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+               
+                <li><a href="#"><i class="fa fa-instagram text-white"></i></a></li>
+
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-2">
+           <ul class="list-inline sm-pull-none sm-text-center text-right text-white mb-sm-20 mt-10">
+              <li class="m-0 pl-0 pr-10"> 
+                <!--<a href="verify.php" class=""><i class="fa fa-edit mr-5"></i>Verify Certificate</a> -->
+
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+     <div class="header-nav">
+      <div class="header-nav-wrapper navbar-scrolltofixed bg-white"  >
+        <div class="container" >
+          <nav id="menuzord-right"  class="menuzord default"><a class="menuzord-brand pull-left flip mt-sm-10 mb-sm-20" href=""><img src="images/hrithik1.png"  style="height:200px;" alt=""></a>
+            <ul class="menuzord-menu">
+			              <li class=""><a href="jammu.php">Jammu</a>
+                </li>
+                  
+                  
+                  
+                <li class=""><a href="kishtwar.html">Kishtwar</a>
+				
+                </li>
+
+              <li><a href="#home">LEH-LADAKH</a>
+                <ul class="dropdown">
+                  <li><a href="leh.php">Leh</a></li>
+                  <li><a href="ladakh.php">Ladakh</a></li>
+                </ul>
+              </li>
+                 <li class=""><a href="kashmir.php">Kashmir</a>
+                </li>
+				<li class="active"><a href="contactus.html">Contact Us</a>
+                </li>
+              
+                </ul>
+              </li>
+              
+              
+                  
+             
+                     
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  
+  <!-- Start main-content -->
+  <div class="main-content">
+
+    <!-- Section: inner-header -->
+    <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="images/gul.jpg">
+      <div class="container pt-120 pb-60">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row"> 
+            <div class="col-md-6">
+              <h2 class="text-theme-colored2 font-36">Kashmir</h2>
+              <ol class="breadcrumb text-left mt-10 white">
+                                <li class="active"></li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <!-- Gallery Grid 3 -->
+ 
+   <section id="team" class="bg-silver-deep">
+      <div class="container">
+        <div class="section-title">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="text-uppercase title">Gallery <span class="text-theme-colored2"></span></h2>              
+              <p class="text-uppercase mb-0">Some of our Glimpses</p>
+					  	<div class="double-line-bottom-theme-colored-2"></div>
+						</div>
+          </div>
+        </div>
+        <div class="row mtli-row-clearfix">
+		<?php
+include("connect.php");
+$r = "SELECT * FROM kashmir";  // table name
+$rt = mysqli_query($cc, $r);
+
+while($res = mysqli_fetch_assoc($rt)) {
+    $pic = "admin/images/" . $res['gallery'];  // image column
+    $title = $res['title'];                   // title column
+    ?>
+    <div class="col-xs-12 col-sm-6 col-md-3">
+        <div class="team-members border-bottom-theme-colored2px text-center maxwidth400 mb-30">
+            <div class="team-thumb">
+                <img class="img-fullwidth" alt="<?= $title; ?>" src="<?= $pic; ?>">
+                <div class="team-overlay"></div>
+            </div>
+            <!-- Title below the image -->
+            <p class="mt-10 font-weight-bold"><?= $title; ?></p>
+        </div>
+    </div>
+<?php
+}
+?>
+
+          
+         
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- end main-content -->
+
+  <!-- Footer -->
+   <footer id="footer" class="footer" data-bg-color="#212331">
+    <div class="container pt-70 pb-40">
+      <div class="row">
+        <div class="col-sm-6 col-md-3">
+          <div class="widget dark">
+            <img class="mt-5 mb-20" alt="" src="images\hrithik2.png">
+            <p>Gandhinagar Gole Market,Jammu</p>
+            <ul class="list-inline mt-5">
+              <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">8492840864</a> </li>
+              <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">hrithiks227@gmail.com</a> </li>
+              
+            </ul>            
+            <ul class="styled-icons icon-sm icon-bordered icon-circled clearfix mt-10">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            
+              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                          </ul>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+          <div class="widget dark">
+            <h4 class="widget-title line-bottom-theme-colored-2">Useful Links</h4>
+            <ul class="angle-double-right list-border">
+              <li><a href="jammu.php">Jammu</a></li>
+              <li><a href="kishtwar.html">Kishtwar</a></li>
+              <li><a href="kashmir.php">Kashmir</a></li>
+              <li><a href="leh.php">Leh</a></li>
+              <li><a href="ladakh.php">Ladakh</a></li> 
+              <li><a href="contactus.html">Contact Us</a></li>
+               
+            </ul>
+          </div>
+        </div>
+         <div class="col-sm-6 col-md-3">
+          <div class="widget dark">
+            <h4 class="widget-title line-bottom-theme-colored-2"> News</h4>
+            <div class="latest-posts">
+              <article class="post media-post clearfix pb-0 mb-10">
+                <a class="post-thumb" href="#"><img src="https://placehold.it/80x55" alt=""></a>
+                <div class="post-right">
+                  <h5 class="post-title mt-0 mb-5"><a href="#">Doeacc</a></h5>
+
+                </div>
+              </article>
+              <article class="post media-post clearfix pb-0 mb-10">
+                <a class="post-thumb" href="#"><img src="https://placehold.it/80x55" alt=""></a>
+                <div class="post-right">
+                  <h5 class="post-title mt-0 mb-5"><a href="#">NSIC</a></h5>
+                  
+                </div>
+              </article>
+              <article class="post media-post clearfix pb-0 mb-10">
+                <a class="post-thumb" href="#"><img src="https://placehold.it/80x55" alt=""></a>
+                
+              </article>
+            </div>
+          </div>
+        </div>
+ <div class="col-sm-6 col-md-3">
+          <div class="widget dark">
+            <h4 class="widget-title line-bottom-theme-colored-2">Opening Hours</h4>
+            <div class="opening-hours">
+              <ul class="list-border">
+                <li class="clearfix"> <span> Mon - Tues :  </span>
+                  <div class="value pull-right"> 9.00 am - 5.00 pm </div>
+                </li>
+                <li class="clearfix"> <span> Wednes - Thurs :</span>
+                  <div class="value pull-right"> 9.00 am - 5.00 pm </div>
+                </li>
+                <li class="clearfix"> <span> Fri -Sat: </span>
+                  <div class="value pull-right"> 9.00 pm - 5.00 pm </div>
+                </li>
+                <li class="clearfix"> <span> Sun : </span>
+                  <div class="value pull-right bg-theme-colored2 text-white closed"> Closed </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+     <div class="footer-bottom" data-bg-color="#2b2d3b">
+      <div class="container pt-20 pb-20">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="font-12 text-black-777 m-0 sm-text-center">Copyright &copy;2024 DOEACC EDGES. All Rights Reserved</p>
+          </div>
+          <div class="col-md-6 text-right">
+            <div class="widget no-border m-0">
+              <ul class="list-inline sm-text-center mt-5 font-12">
+                <li>
+                  <a href="#">Site Developed by: Techno Solutions</a>
+                </li>
+                <li>|</li>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/custom.js"></script>
+
+</body>
+
+<!-- Mirrored from html.kodesolution.com/2017/learnpro-html/demo/page-gallery-3col.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Aug 2025 13:40:12 GMT -->
+</html>
